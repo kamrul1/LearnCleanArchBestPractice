d157e2c645d2fde46d53856eb591bc94f58aaf60
kamrul.islam <kamrul.islam@dotnet-specialist.co.uk>
2021-11-07 13:44:41 +0000

    Add project files.

---
+++ GlobalTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using GlobalTicket.TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    public class Category: AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+
+    }
+}
---
+++ build/README.md
@@ -0,0 +1,6 @@
+﻿# Architecting ASP.NET Core Applications: Best Practices
+
+These are my course notes following the [pluralsight course](https://app.pluralsight.com/library/courses/architecting-asp-dot-net-core-applications-best-practices/table-of-contents)
+
+
+
